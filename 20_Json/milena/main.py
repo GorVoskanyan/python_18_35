9b780d50938703956a02fa5d383abf413f77ae68
Milena <milenapapoyan675@gmail.com>
2024-09-11 14:43:46 +0400

    Milen modified her task

--- 20_Json/milena/main.py
+++ 20_Json/milena/main.py
@@ -1,23 +1,30 @@
 import json
 import requests
 
-URL = "https://swapi.dev/api/people/"
+BASEURL = "https://swapi.dev/api/people/"
 
-brownsData = dict()
+eyeData = dict()
+
 for endpoint in range(1, 84):
     print("Information about: ", endpoint)
+    
     if endpoint == 17:
         continue
+    
     try:
-        endpoint = str(endpoint)
-        URL += endpoint + "/"
-        peopleInfo = requests.get(URL).json()
+        URL = BASEURL + str(endpoint) + "/"
+        response = requests.get(URL)
+
+        peopleInfo = response.json()
         
-        if peopleInfo["eye_color"] == "blue":
-            brownsData[peopleInfo["name"]] = peopleInfo["eye_color"]
+        if peopleInfo["eye_color"] == "yellow":
+            eyeData[peopleInfo["name"]] = peopleInfo["eye_color"]
+            
     except requests.exceptions.JSONDecodeError as message:
         print(message)
-print(brownsData)
+    except json.decoder.JSONDecodeError as message:
+        print(message)
+
 
 with open("eyedata.json", "w", encoding = "utf-8") as jsonFile:
-    json.dump(brownsData, jsonFile, indent = 4)
+    json.dump(eyeData, jsonFile, indent = 4)
